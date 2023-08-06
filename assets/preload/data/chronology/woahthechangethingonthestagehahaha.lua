b8b73a4e50353ced1c3fe5dc260b29584d44a8df
Shaggers <105822831+Shaggers-official@users.noreply.github.com>
2023-08-06 11:30:30 +0100

    just some stuff

--- assets/preload/data/chronology/woahthechangethingonthestagehahaha.lua
+++ assets/preload/data/chronology/woahthechangethingonthestagehahaha.lua
@@ -1,6 +1,37 @@
 local stuff = {
-    {songTitle = 'chronology', opponentColor = '3fe780', playerColor = 'a357ab', gfColor = 'd4363c'}
+    {songTitle = 'chronology', opponentColor = '3fe780', playerColor = 'a357ab', gfColor = 'd4363c', composers = {"Smily, KOSE"}}
 }
+
+function onCreate()
+    for _, data in ipairs(stuff) do
+        if data.songTitle == songName then
+            curComposer = table.concat(data.composers, ", ")
+
+            makeLuaSprite('songSign', 'songSign', -400, 175)
+            setObjectCamera('songSign', 'camOther')
+            setProperty('songSign.antialiasing', false)
+            addLuaSprite('songSign', false)
+
+            makeLuaSprite('shaggersSign', 'shaggersSign', -400, 300)
+            setObjectCamera('shaggersSign', 'camOther')
+            setProperty('shaggersSign.antialiasing', false)
+            addLuaSprite('shaggersSign', false)
+
+            makeLuaText('comp', songName..'\n'..curComposer, 300, 0, 0)
+            setObjectCamera('comp', 'camOther')
+            setTextAlignment('comp', 'left')
+            setTextSize('comp', 36)
+            addLuaText('comp', false)
+
+            makeLuaText('shag', 'Coded by\nShaggers', 300, 0, 0)
+            setObjectCamera('shag', 'camOther')
+            setTextAlignment('shag', 'left')
+            setTextSize('shag', 36)
+            addLuaText('shag', false)
+        end
+    end
+    precacheSound('boop')
+end
 
 function onSongStart()
     for _, data in ipairs(stuff) do
@@ -11,7 +42,69 @@ function onSongStart()
     end
 end
 
+function onUpdate(elapsed)
+    setProperty('comp.x', getProperty('songSign.x') + 10)
+    setProperty('shag.x', getProperty('shaggersSign.x') + 10)
+    setProperty('comp.y', getProperty('songSign.y') + 10)
+    setProperty('shag.y', getProperty('shaggersSign.y') + 10)
+end
+
+function onUpdatePost(elapsed)
+    if getPropertyFromClass('flixel.FlxG', 'keys.justPressed.SEVEN') or getPropertyFromClass('flixel.FlxG', 'keys.justPressed.EIGHT') or getPropertyFromClass('flixel.FlxG', 'keys.justPressed.NUMPADMULTIPLY')then
+        playSound('boop', 1, 'boop')
+    end
+
+    for _, data in ipairs(stuff) do
+        if data.songTitle == songName then
+            if getPropertyFromClass('flixel.FlxG', 'mouse.visible') == true then
+                if mouseOverLapsSprite('shag', 'other') then
+                    setTextColor('shag', data.opponentColor)
+                    if mouseClicked() then
+                        os.execute('start "" "https://youtube.com/@Shaggers"')
+                    end
+                elseif mouseOverLapsSprite('comp', 'other') then
+                    setTextColor('comp', data.opponentColor)
+                    if mouseClicked() then
+                        os.execute('start "" "https://www.youtube.com/@boi_smily"')
+                        os.execute('start "" "https://www.youtube.com/@protonatedOH"')
+                    end
+                else
+                    setTextColor('comp', 'FFFFFF')
+                    setTextColor('shag', 'FFFFFF')
+                end
+            else
+                setTextColor('comp', 'FFFFFF')
+                setTextColor('shag', 'FFFFFF')
+            end
+        end
+    end
+end
+
+function onTweenCompleted(tag)
+    if tag == 'songsignbye' then
+        removeLuaSprite('songSign', true)
+        removeLuaSprite('shaggersSign', true)
+        removeLuaText('comp', true)
+        removeLuaText('shag', true)
+    end
+end
+
+function onTimerCompleted(tag)
+    if tag == 'byebye' then
+        doTweenX('songsignbye', 'songSign', -400, crochet / 200, 'quadInOut')
+        doTweenX('shagsignbye', 'shaggersSign', -400, crochet / 200, 'quadInOut')
+        setPropertyFromClass('flixel.FlxG', 'mouse.visible', false)
+    end
+end
+
 function onStepHit()
+    if curStep == 256 then
+        setPropertyFromClass('flixel.FlxG', 'mouse.visible', true)
+        doTweenX('songsign', 'songSign', 50, crochet / 500, 'quadInOut')
+        doTweenX('shagsign', 'shaggersSign', 50, crochet / 500, 'quadInOut')
+        runTimer('byebye', 4)
+    end
+    
     if curStep == 506 then
         setProperty('defaultCamZoom', 0.9)
         setProperty('cameraSpeed', 1000)
@@ -24,12 +117,12 @@ function onStepHit()
     end
 
     if curStep == 704 or curStep == 720 then
-        cameraFlash('camGame', 'FFFFFF', (crochet / 1000) * 4)
+        cameraFlash('camGame', 'FFFFFF', (crochet / 1000) * 3)
         setProperty('defaultCamZoom', getProperty('defaultCamZoom') + 0.1)
     end
 
     if curStep == 736 or curStep == 744 or curStep == 752 then
-        cameraFlash('camGame', 'FFFFFF', (stepCrochet / 1000) * 8)
+        cameraFlash('camGame', 'FFFFFF', (stepCrochet / 1000) * 7)
         setProperty('defaultCamZoom', getProperty('defaultCamZoom') + 0.05)
     end
 
@@ -220,4 +313,29 @@ function onSectionHit()
             end
         end
     end
+end
+
+function onDestroy()
+    setPropertyFromClass('flixel.FlxG', 'mouse.visible', false)
+end
+
+function posOverlaps(
+    x1, y1, w1, h1, --r1,
+    x2, y2, w2, h2 --r2
+)
+    return (
+        x1 + w1 >= x2 and x1 < x2 + w2 and
+        y1 + h1 >= y2 and y1 < y2 + h2
+    )
+end
+
+function mouseOverLapsSprite(spr, cam)
+    local mouseX, mouseY = getMouseX(cam or "other"), getMouseY(cam or "other")
+    
+    local x, y, w, h = getProperty(spr .. ".x"), getProperty(spr .. ".y"), getProperty(spr .. ".width"), getProperty(spr .. ".height")
+    
+    return posOverlaps(
+        mouseX, mouseY, 1, 1,
+        x, y, w, h
+    )
 end
