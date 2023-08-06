55f02963b4bf60b3edeca800aa02069ae1b5718a
Shaggers <105822831+Shaggers-official@users.noreply.github.com>
2023-08-06 03:33:28 +0100

    Initial commit

---
+++ assets/preload/characters/ourpleplay.lua
@@ -0,0 +1,65 @@
+function onCreate()
+  	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'ourple_death')
+end
+
+flipped = true
+flippedIdle = false
+defaultY = 0
+
+function onCreatePost()
+	defaultY = getProperty('boyfriend.y')
+end
+
+function onSongStart()
+	if songName == 'cronology' then
+		defaultY = getProperty('boyfriend.y')
+	end
+end
+
+function onBeatHit() 
+	if getProperty('healthBar.percent') > 20 then
+		flipped = not flipped
+		setProperty('iconP1.flipX', flipped)
+	end
+	
+	if curBeat % 1 == 0 and getProperty('boyfriend.animation.curAnim.name') == 'idle' or getProperty('boyfriend.animation.curAnim.name') == 'danceLeft' or getProperty('boyfriend.animation.curAnim.name') == 'danceRight' then
+		setProperty('boyfriend.y', getProperty('boyfriend.y') + 20)
+		doTweenY('raise', 'boyfriend', getProperty('boyfriend.y') - 20, 0.15, 'cubeOut')
+		if boyfriendName == 'ourpleplay' then
+			flippedIdle = not flippedIdle
+			setProperty('boyfriend.flipX', flippedIdle)
+		end
+	end
+end
+
+function goodNoteHit(id, direction, noteType, isSustainNote)
+	if not getPropertyFromGroup('notes', id, 'gfNote') then
+		cancelTween('raise')
+		setProperty('boyfriend.y', defaultY)
+		setProperty('boyfriend.flipX', true)
+	end
+end
+
+function noteMiss(id, direction, noteType, isSustainNote)
+	if not getPropertyFromGroup('notes', id, 'gfNote') then
+		cancelTween('raise')
+		setProperty('boyfriend.y', defaultY)
+		setProperty('boyfriend.flipX', true)
+	end
+end
+
+function onStepHit() 
+	if getProperty('healthBar.percent') < 20 and curStep % 2 == 0 then
+		flipped = not flipped
+		setProperty('iconP1.flipX', flipped)
+	end
+end
+
+function onUpdate(e)
+	local angleOfs = math.random(-5, 5)
+	if getProperty('healthBar.percent') < 20 then
+		setProperty('iconP1.angle', angleOfs)
+	else
+		setProperty('iconP1.angle', 0)
+	end
+end
